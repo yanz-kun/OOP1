b1ef29c006f2625ee2e461c5a4117aa6018604e9
yanz-kun <56082058+yanz-kun@users.noreply.github.com>
2020-03-21 19:42:46 +0700

    Add files via upload

---
+++ OOP1/src/oop1/Mahasiswa.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop1;
+
+/**
+ *
+ * @author Yanuar Ardani N
+ */
+public class Mahasiswa {
+    
+    public static void main(String[] args) {
+      String nama = "Yanuar Ardani Nurullah";
+      String kelas = "4A";
+      String nim = "18090031";
+        System.out.println("Nama : "+nama);
+        System.out.println("Kelas : "+kelas);
+        System.out.println("Nim : "+nim);
+        
+    }
+    
+}
